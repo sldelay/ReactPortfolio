b67bdea679bc51dce671ef307cdd56706decbf28
Sam Delay <samuel.delay@gmail.com>
2020-05-17 19:49:26 -0400

    navbar

---
+++ my-app/src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from 'react-bootstrap/Nav'
+
+function NavComp() {
+
+
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="/">Samuel Delay</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+        <Nav>
+          <Nav.Link href="/About">About</Nav.Link>
+          <Nav.Link href="/Portfolio">Portfolio</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+
+}
+
+export default NavComp;
