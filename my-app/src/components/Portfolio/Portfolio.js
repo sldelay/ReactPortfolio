da5041d50872fd5a02192e5dc024776ed2f1e8d0
Sam Delay <samuel.delay@gmail.com>
2020-05-17 19:49:40 -0400

    portfolio "page" component

---
+++ my-app/src/components/Portfolio/Portfolio.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./style.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import ProCard from "../ProjectCard/Card";
+import Footer from "../Footer/Footer";
+
+const Portfolio = ({ projects }) => {
+  console.log(projects)
+  return (
+    <>
+      <Container className="Portfolio">
+        <Row>
+          {projects.map((project) => (
+            <ProCard key={project.id} project={project} />
+          ))}
+        </Row>
+      </Container>
+      <Footer />
+    </>
+  )
+}
+
+export default Portfolio;
