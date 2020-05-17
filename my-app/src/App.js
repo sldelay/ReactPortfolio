7a3738fd461634b79167b8f0194cc55818483083
Sam Delay <samuel.delay@gmail.com>
2020-05-17 19:48:20 -0400

    app.js

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavComp from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
+import projects from "./projects.json";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+    <NavComp />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/About" component={About} />
+      </Switch>
+      <Switch>
+        <Route exact path="/Portfolio">
+          <Portfolio projects={projects}/>
+        </Route>
+      </Switch>
+    </Router>
+    </>
+  );
+}
+
+export default App;
